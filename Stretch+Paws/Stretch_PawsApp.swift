7b129f0ca724242b12cc4b557356bdae774109a6
Alex Kirts <alex.kirts@gmail.com>
2021-12-12 17:49:39 -0700

    Initial Commit

---
+++ Stretch+Paws/Stretch_PawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stretch_PawsApp.swift
+//  Stretch+Paws
+//
+//  Created by Alex Kirts on 12/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Stretch_PawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
